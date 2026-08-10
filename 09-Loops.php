90345534e5da0a4fcb0837f233693a861d6a2711
Samuel Souza <ss244112@gmail.com>
2026-08-10 09:39:01 -0300

    adicionando div no loop

--- 09-Loops.php
+++ 09-Loops.php
@@ -7,6 +7,7 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
 </head>
 <body>
+    <div class="container"></div>
     <h1>Trabalhando com comandos de repetição</h1>
     <hr>
 
@@ -54,7 +55,7 @@
             </details>
             <?php } // ou endfor; ?>
           </section>
-
+            </div>
            <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.min.js" integrity="sha384-G/EV+4j2dNv+tEPo3++6LCgdCROaejBqfUeNjuKAiuXbjrxilcCdDz6ZAVfHWe1Y" crossorigin="anonymous"></script>
 </body>
 </html>
