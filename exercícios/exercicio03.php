3786902d40ccfdd775d048456cfbfb6b0869e010
Samuel Souza <ss244112@gmail.com>
2026-08-10 11:33:14 -0300

    foram adicionados a pasta exercícios

---
+++ exercícios/exercicio03.php
@@ -0,0 +1,64 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Condicionais e uso do PHP intercalado com HTML</title>
+    <style>
+        h1{ background-color: wheat;}
+        body{text-align: center;}
+       
+         ul{text-align: center;
+         list-style-type: none;
+         padding: 4px;
+         margin: auto;
+         width: 50%;
+         border: solid 1px;}
+         .crianca{background-color: royalblue;}
+         .adulto{background-color: wheat;}
+         .melhor-idade{background-color: pink;}  
+        
+       
+
+    </style>
+</head>
+<body>
+    <h1>===== Ingressos show shaulin do sertão =====</h1>
+    <hr>
+   
+    <?php 
+    $idade = 65;
+    $categoria = "";
+    $valor = "";
+
+    if($idade <= 12){
+       $categoria = "Criança";
+       $valor = 25.00;
+       $estilo = "crianca";
+    } elseif ($idade <=60){
+      $categoria ="Adulto";
+      $valor = 40.00;
+      $estilo = "adulto";
+
+      }else{
+      $categoria= "Melhor idade";
+      $valor = 20.00; 
+      $estilo = "melhor-idade";
+      }
+      ?>
+
+      <hr>
+
+       <ul class="<?= $estilo ?>">
+        <li>Idade pessoa: <?= $idade ?> </li>
+        <li>A categoria do ingresso: <?= $categoria ?> </li>
+        <li>O valor do  ingresso: R$ <?= number_format($valor, 2, "," ,".") ?> </li>
+    </ul>
+
+    <hr>
+
+
+
+ <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.min.js" integrity="sha384-G/EV+4j2dNv+tEPo3++6LCgdCROaejBqfUeNjuKAiuXbjrxilcCdDz6ZAVfHWe1Y" crossorigin="anonymous"></script>
+</body>
+</html>
---
+++ exercícios/exercicio04.php
@@ -0,0 +1,85 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>estruturas de repetição (loops) e estruturas de dados</title>
+     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
+</head>
+<body> 
+    <div class="container">
+      <h1>Estruturas de repetição (loops) e estruturas de dados</h1>
+      
+      <?php
+           $linguagens = ["HTML","CSS","JS","PHP","SQL"];
+      ?>
+
+       <ol>
+        <?php for ($i = 0; $i < count ($linguagens); $i++):?>
+
+
+        
+        <?php endfor ?>
+        
+       </ol>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    </div>
+      
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+ <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.min.js" integrity="sha384-G/EV+4j2dNv+tEPo3++6LCgdCROaejBqfUeNjuKAiuXbjrxilcCdDz6ZAVfHWe1Y" crossorigin="anonymous"></script> 
+</body>   
+ </html>
+
