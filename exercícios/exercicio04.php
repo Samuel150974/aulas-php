4154763e5ad9007b9ba10301146fe36f6067b35d
Samuel Souza <ss244112@gmail.com>
2026-08-12 10:32:59 -0300

    atividade exercicio 04
    correção com apresentação de tags TD,TR, TABLE E TH

--- exercícios/exercicio04.php
+++ exercícios/exercicio04.php
@@ -1,59 +1,76 @@
 <!DOCTYPE html>
 <html lang="pt-br">
+
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>estruturas de repetição (loops) e estruturas de dados</title>
-     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>estruturas de repetição (loops) e estruturas de dados</title>
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
 </head>
-<body> 
-    <div class="container">
-      <h1>Estruturas de repetição (loops) e estruturas de dados</h1>
+
+<body>
+  <div class="container">
+    <h1>Estruturas de repetição (loops) e estruturas de dados</h1>
+
+    <?php
+    $linguagens = [
+      [
+        "id" => 1,
+        "nome" => "HTML",
+        "descricao" => "Estruturação"
+      ],
+
+      [
+        "id" => 2,
+        "nome" => "CSS",
+        "descricao" => "Estilos"
+      ],
+
+      [
+        "id" => 3,
+        "nome" => "JS",
+        "descricao" => "Comportamentos"
+      ],
+
+      [
+        "id" => 4,
+        "nome" => "PHP",
+        "descricao" => "Back-End"
+      ],
+
+      [
+        "id" => 5,
+        "nome" => "SQL",
+        "descricao" => "Manipulação de dados"
+      ]
+    ];
+
+    ?>
+
+
+
+      <table class="table table-bordered table-striped table-hover">
+        <thead class="table-dark">
+          <tr>
+            <th>id</th> <!-- tr É usada para criar linhas dentro de uma tabela HTML. -->
+            <th>nome</th> <!-- //th É usada dentro de uma tabela (<table>) para definir células de cabeçalho. -->
+            <th>descrição</th>
+          </tr>
+        </thead>
       
-      <?php
-           $linguagens = [ 
-           [
-           "id" =>1,
-           "nome"=>"HTML", 
-           "descrição" => "Estruturação" 
-           ],
+      <tbody>
+        <?php foreach ($linguagens as $linguagem): ?>
 
-           [
-           "id" =>2,
-           "nome"=>"CSS", 
-           "descrição" => "Estilos" 
-           ],
+          <tr>
+            <td><?= $linguagem['id'] ?></td>
+            <td><?= $linguagem['nome'] ?></td>
+            <td><?= $linguagem['descricao'] ?></td>
+          </tr>
+        <?php endforeach ?>
 
-           [
-           "id" =>3,
-           "nome"=>"JS", 
-           "descrição" => "Comportamentos" 
-           ],
+        </tbody>
 
-           [
-           "id" =>4,
-           "nome"=>"PHP", 
-           "descrição" => "Back-End" 
-           ],
-           
-           [
-           "id" =>5,
-           "nome"=>"SQL", 
-           "descrição" => "Manipulação de dados" 
-           ]
-           ];
-     
-        foreach($linguagens as $chaves => $valores):
-          ?>
-          
-          <?php
-          endforeach;
-
-          foreach($linguagens as $valores): 
-          ?>
-           
-
-      
+      </table>
 
 
 
@@ -74,11 +91,7 @@
 
 
 
-       
-
-
-    </div>
-      
+  </div>
 
 
 
@@ -112,7 +125,8 @@
 
 
 
- <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.min.js" integrity="sha384-G/EV+4j2dNv+tEPo3++6LCgdCROaejBqfUeNjuKAiuXbjrxilcCdDz6ZAVfHWe1Y" crossorigin="anonymous"></script> 
-</body>   
- </html>
 
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.min.js" integrity="sha384-G/EV+4j2dNv+tEPo3++6LCgdCROaejBqfUeNjuKAiuXbjrxilcCdDz6ZAVfHWe1Y" crossorigin="anonymous"></script>
+</body>
+
+</html>
