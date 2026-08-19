95d77b17a277001d6868181f433675f10dc1ab75
Samuel Souza <ss244112@gmail.com>
2026-08-19 11:59:23 -0300

    linkando páginas usando PHP

---
+++ site/includes/cursos.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
+    <title>Site usando php</title>
+        <?php require "includes/cabecalho.php"?>
+</head>
+<body>
+
+ 
+<main>
+    <h2>Página de cursos</h2>
+    <p>Veja abaixo nossos cursos: </p>
+    <ul>
+        <li>HTML</li>
+        <li>JS</li>
+    </ul>
+</main>
+
+    <?php require "includes/rodape.php"?>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js" integrity="sha384-FKyoEForCGlyvwx9Hj09JcYn3nv7wiPVlz7YYwJrWVcXK/BmnVDxM+D2scQbITxI" crossorigin="anonymous"></script>
+
+
+
+</body>
+</html>
