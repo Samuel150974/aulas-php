3aed533900068834069d00722559ff68cb9703ed
Samuel Souza <ss244112@gmail.com>
2026-08-24 08:44:30 -0300

    revisão $_GET e $_POST

--- processamento.php
+++ processamento.php
@@ -1,46 +1,52 @@
 <!DOCTYPE html>
-<html lang="en">
-
+<html lang="pt-br">
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
-
-    <title>Document</title>
-
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<title>Processamento dos Dados</title>
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
 </head>
-
 <body>
-    <div class="container">
-        <h1>Recebimento e processamento dos dados</h1>
-        <hr>
-        <?php             //GET exibe informações no link de buscas
-  
-//Capturando os dados de cada campo
-        $nome = $_POST["nome"];
-        $email = $_POST["email"];
-        $idade = $_POST["idade"];
-        $mensagem = $_POST["mensagem"];
-          /* operador ?? -> Caso nenhum interesse seja selecionado, a variável guardará um array vazio*/
-        $interesses = $_POST["interesses"]?? []; //array
-        
-        //Caso nenhuma opção seja selecionada, o valor "nao" fica como padrão
-        $informativos = $_POST["informativos"];
-        ?>
+<div class="container">
+    <h1>Recebimento e processamento dos dados</h1>
+    <hr>
 
-         <h2>Dados recebidos</h2>
-         <p>Nome: <?= $nome ?></p>
-         <p>Email: <?= $email ?></p>
-         <p>Idade: <?= $idade ?></p>
-         <p>Mensagem: <?= $mensagem ?></p>
-         <?php if(!empty($interesses)):   ?>
-         <p>Interesses: <?= implode(", ", $interesses) ?></p>
-         <?php endif; ?>
+<?php 
+/* $_POST e $_GET
+Arrays superglobais que possuem os dados
+enviados a partir de formulários e/ou
+links dinâmicos. */
 
-         
-         <p>Informativos: <?= $informativos ?></p>
-    </div>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.min.js" integrity="sha384-G/EV+4j2dNv+tEPo3++6LCgdCROaejBqfUeNjuKAiuXbjrxilcCdDz6ZAVfHWe1Y" crossorigin="anonymous"></script>
+// Capturando os dados de cada campo
+$nome = $_POST["nome"];
+$email = $_POST["email"];
+$idade = $_POST["idade"];
+$mensagem = $_POST["mensagem"];
+
+/* Operador ?? -> coalescência nula
+Caso nenhum interesse seja selecionado, 
+a variável guardará um array vazio */
+$interesses = $_POST["interesses"] ?? []; 
+
+// Caso nenhuma opção seja selecionada, o valor "nao" fica como padrão
+$informativos = $_POST["informativos"] ?? "nao";
+?>   
+    <h2>Dados recebidos</h2>
+    <p>Nome: <?= $nome ?></p>
+    <p>E-mail: <?= $email ?></p>
+    <p>Idade: <?= $idade ?> anos</p>
+    <p>Mensagem: <?= $mensagem ?> </p>
+    
+    <?php if(!empty($interesses)): ?>
+    <p>Interesses: <?= implode(", ", $interesses) ?></p>
+    <?php endif; ?>
+
+    <p>Informativos: 
+        <?= $informativos === 'sim' ? "Sim" : "Não" ?>
+    </p>
+
+</div>
+
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js" integrity="sha384-FKyoEForCGlyvwx9Hj09JcYn3nv7wiPVlz7YYwJrWVcXK/BmnVDxM+D2scQbITxI" crossorigin="anonymous"></script>
 </body>
-
 </html>
